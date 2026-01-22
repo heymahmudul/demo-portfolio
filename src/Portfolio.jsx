d7919f95d64d5ae9e4daef11fe5fc4ca2a1a449b
Sk Mahmudul Haque <heymahmudul@gmail.com>
2026-01-22 17:17:19 +0530

    Add Portfolio.jsx component

---
+++ src/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>My Portfolio</h1>
+            <p>Welcome to my portfolio website!</p>
+        </div>
+    );
+};
+
+export default Portfolio;
