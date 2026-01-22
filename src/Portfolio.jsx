6340ada4fe00c03b3470304c5a1893ec2d602ba5
Sk Mahmudul Haque <heymahmudul@gmail.com>
2026-01-22 17:18:23 +0530

    Create Portfolio.jsx file for the portfolio component

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -5,6 +5,7 @@ const Portfolio = () => {
         <div>
             <h1>My Portfolio</h1>
             <p>Welcome to my portfolio website!</p>
+            {/* Add more components and sections as needed */}
         </div>
     );
 };
